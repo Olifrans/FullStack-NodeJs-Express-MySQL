630c394f0e34421801ff9c148b08a3c59ba7dfc0
Francisco das Chagas de Oliveira <f.chagas@itinovacao.onmicrosoft.com>
2022-05-25 11:07:06 -0300

    feat: Crud get e Post ok

--- server/index.js
+++ server/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const app = express();
 
+// const cor = require("cors");
+
+app.use(express.json());
+// app.use(cor());
+
+
 const db = require("./models");
 
 
+
+//Routers - Post
+const postRouter = require("./routes/Posts");
+app.use("/posts", postRouter);
+
+// //Routers - Comments
+// const commentsRouter = require("./routes/Comments");
+// app.use("/comments", commentsRouter);
 
 
 
